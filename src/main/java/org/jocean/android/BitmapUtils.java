94e4bb6ac2fe59d5a4c5adc566d74f0eaf34d27d
ming.ma <ming.ma@upai.com>
2014-04-24 11:29:40 +0800

    升级到 0.0.2-SNAPSHOT，新增 BitmapUtils，提供 simpleimage-lite中的 JPEGDecoder 解码 "image/jpeg" 能力，解决 android native image decode 不能识别 CMYK 颜色空间的 jpeg 图片问题

---
+++ src/main/java/org/jocean/android/BitmapUtils.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package org.jocean.android;
+
+import java.io.InputStream;
+
+import org.jocean.idiom.Triple;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
+
+import com.alibaba.simpleimage.codec.jpeg.JPEGDecoder;
+import com.alibaba.simpleimage.io.ImageBitsInputStream;
+
+/**
+ * @author isdom
+ *
+ */
+public class BitmapUtils {
+    
+    private static final Logger LOG = 
+            LoggerFactory.getLogger(BitmapUtils.class);
+
+    public static String parseImageMimeType(final InputStream is) throws Exception {
+        final BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inJustDecodeBounds = true; //确保图片不加载到内存
+        
+        BitmapFactory.decodeStream(is, null, opts);
+        
+        return opts.outMimeType;
+    }
+    
+    public static Bitmap decodeStreamByMimeType(final InputStream is, final String mimeType) 
+        throws Exception {
+        if (  mimeType.equals("image/jpeg") ) {
+            LOG.info("using simpleimage.JPEGDecoder");
+            
+            final JPEGDecoder decoder = new JPEGDecoder(new ImageBitsInputStream(is));
+            final Triple<Integer, Integer, int[]> rawimg = decoder.decode();
+            if ( null != rawimg ) {
+                return Bitmap.createBitmap(rawimg.getThird(), rawimg.getFirst(), rawimg.getSecond(), Config.ARGB_8888);
+            }
+            else {
+                LOG.warn("simpleimage.JPEGDecoder failed.");
+                return null;
+            }
+        }
+        else {
+            LOG.info("using BitmapFactory.decodeStream");
+            
+            return BitmapFactory.decodeStream(is);
+        }
+    }
+    
+}
