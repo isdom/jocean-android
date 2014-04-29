21e405eaa109c413caa74c59dd42886b7eeffcd2
ming.ma <ming.ma@upai.com>
2014-04-29 15:55:11 +0800

    基于simpleimage-lite的全流式JPEG解码(参见commit:https://github.com/isdom/simpleimage-lite/commit/d6eae5e0321218eafd006f70629505aa2a08891d)，修改BitmapUtils的decodeStream实现，通过bitmap.setPixel方法绘制JPEGDecoder的IntsBlob方式返回的解码结果

--- src/main/java/org/jocean/android/BitmapUtils.java
+++ src/main/java/org/jocean/android/BitmapUtils.java
@@ -7,7 +7,10 @@ import java.io.InputStream;
 
 import org.jocean.idiom.ExceptionUtils;
 import org.jocean.idiom.Triple;
+import org.jocean.idiom.block.IntsBlob;
+import org.jocean.idiom.block.ReadableInts;
 import org.jocean.idiom.pool.BytesPool;
+import org.jocean.idiom.pool.IntsPool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,6 +30,17 @@ public class BitmapUtils {
     private static final Logger LOG = 
             LoggerFactory.getLogger(BitmapUtils.class);
 
+    public static final class Context {
+        
+        public Context(final BytesPool bytesPool, final IntsPool intsPool ) {
+            this.bytesPool = bytesPool;
+            this.intsPool = intsPool;
+        }
+        
+        final BytesPool bytesPool;
+        final IntsPool intsPool;
+    }
+    
     public static String parseImageMimeType(final InputStream is) throws Exception {
         final BitmapFactory.Options opts = new BitmapFactory.Options();
         opts.inJustDecodeBounds = true; //确保图片不加载到内存
@@ -36,12 +50,12 @@ public class BitmapUtils {
         return opts.outMimeType;
     }
     
-    public static BitmapHolder decodeStream(final BytesPool pool, final InputStream is) 
+    public static BitmapHolder decodeStream(final Context ctx, final InputStream is) 
             throws Exception {
-        return decodeStream(pool, is, null);
+        return decodeStream(ctx, is, null);
     }
         
-    public static BitmapHolder decodeStream(final BytesPool pool, final InputStream is, final BitmapFactory.Options opts) 
+    public static BitmapHolder decodeStream(final Context ctx, final InputStream is, final BitmapFactory.Options opts) 
         throws Exception {
         if ( !is.markSupported() ) {
             // input stream 不支持mark, 则直接返回 null
@@ -51,26 +65,33 @@ public class BitmapUtils {
         is.mark(0);
         final String mimeType = parseImageMimeType(is);
         is.reset();
-        return decodeStreamByMimeType(pool, is, mimeType, opts);
+        return decodeStreamByMimeType(ctx, is, mimeType, opts);
     }
     
-    public static BitmapHolder decodeStreamByMimeType(final BytesPool pool, final InputStream is, final String mimeType) 
+    public static BitmapHolder decodeStreamByMimeType(final Context ctx, final InputStream is, final String mimeType) 
         throws Exception {
-        return decodeStreamByMimeType(pool, is, mimeType, null);
+        return decodeStreamByMimeType(ctx, is, mimeType, null);
     }
     
-    public static BitmapHolder decodeStreamByMimeType(final BytesPool pool, final InputStream is, final String mimeType, 
+    public static BitmapHolder decodeStreamByMimeType(final Context ctx, final InputStream is, final String mimeType, 
             final BitmapFactory.Options opts) 
         throws Exception {
         if (  mimeType.equals("image/jpeg") ) {
             LOG.info("using simpleimage.JPEGDecoder");
             
             try {
-                final JPEGDecoder decoder = new JPEGDecoder(pool, new ImageBitsInputStream(is));
-                final Triple<Integer, Integer, int[]> rawimg = decoder.decode();
+                final JPEGDecoder decoder = new JPEGDecoder(ctx.bytesPool, new ImageBitsInputStream(is));
+                final Triple<Integer, Integer, IntsBlob> rawimg = decoder.decode(ctx.intsPool);
                 if ( null != rawimg ) {
-                    return new DefaultBitmapHolder(
-                            Bitmap.createBitmap(rawimg.getThird(), rawimg.getFirst(), rawimg.getSecond(), Config.ARGB_8888));
+                    final Bitmap bitmap = 
+                            Bitmap.createBitmap(rawimg.getFirst(), rawimg.getSecond(), Config.ARGB_8888);
+                    final ReadableInts ints = IntsBlob.Utils.releaseAndGenReadable(rawimg.getThird());
+                    for ( int y = 0; y < rawimg.getSecond(); y++) {
+                        for ( int x = 0; x < rawimg.getFirst(); x++) {
+                            bitmap.setPixel(x, y, ints.read());
+                        }
+                    }
+                    return new DefaultBitmapHolder(bitmap);
                 }
             }
             catch (Exception e) {
