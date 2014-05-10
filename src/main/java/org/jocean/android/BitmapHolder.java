60cdf6d6b1c15997b89e29e027d7298a6b9ee56f
ming.ma <ming.ma@upai.com>
2014-05-10 16:22:12 +0800

    移除废弃类 BitmapHolder & DefaultBitmapHolder ，相应移除 BitmapUtils 中的涉及静态方法

--- src/main/java/org/jocean/android/BitmapHolder.java
+++
@@ -1,9 +0,0 @@
-package org.jocean.android;
-
-import org.jocean.idiom.ReferenceCounted;
-
-import android.graphics.Bitmap;
-
-public interface BitmapHolder extends ReferenceCounted<BitmapHolder> {
-    public Bitmap bitmap();
-}
--- src/main/java/org/jocean/android/BitmapUtils.java
+++ src/main/java/org/jocean/android/BitmapUtils.java
@@ -7,10 +7,7 @@ import java.io.InputStream;
 
 import org.jocean.idiom.pool.BytesPool;
 import org.jocean.idiom.pool.IntsPool;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
 /**
@@ -19,9 +16,6 @@ import android.graphics.BitmapFactory;
  */
 public class BitmapUtils {
     
-    private static final Logger LOG = 
-            LoggerFactory.getLogger(BitmapUtils.class);
-
     public static final class Context {
         
         public Context(final BytesPool bytesPool, final IntsPool intsPool ) {
@@ -40,70 +34,5 @@ public class BitmapUtils {
         BitmapFactory.decodeStream(is, null, opts);
         
         return opts.outMimeType;
-    }
-    
-    public static BitmapHolder decodeStream(final Context ctx, final InputStream is) 
-            throws Exception {
-        return decodeStream(ctx, is, null);
-    }
-        
-    public static BitmapHolder decodeStream(final Context ctx, final InputStream is, final BitmapFactory.Options opts) 
-        throws Exception {
-        if ( !is.markSupported() ) {
-            // input stream 不支持mark, 则直接返回 null
-            return null;
-        }
-        
-        is.mark(0);
-        final String mimeType = parseImageMimeType(is);
-        is.reset();
-        return decodeStreamByMimeType(ctx, is, mimeType, opts);
-    }
-    
-    public static BitmapHolder decodeStreamByMimeType(final Context ctx, final InputStream is, final String mimeType) 
-        throws Exception {
-        return decodeStreamByMimeType(ctx, is, mimeType, null);
-    }
-    
-    public static BitmapHolder decodeStreamByMimeType(final Context ctx, final InputStream is, final String mimeType, 
-            final BitmapFactory.Options opts) 
-        throws Exception {
-        /*
-        if (  mimeType.equals("image/jpeg") ) {
-            LOG.info("using simpleimage.JPEGDecoder");
-            
-            try {
-                final JPEGDecoder decoder = new JPEGDecoder(ctx.bytesPool, new ImageBitsInputStream(is));
-                final RawImage rawimg = decoder.decode(ctx.intsPool);
-                if ( null != rawimg ) {
-                    final Bitmap bitmap = 
-                            Bitmap.createBitmap(rawimg.getWidth(), rawimg.getHeight(), Config.ARGB_8888);
-                    final ReadableInts ints = IntsBlob.Utils.releaseAndGenReadable(rawimg.);
-                    try {
-                        for ( int y = 0; y < rawimg.getSecond(); y++) {
-                            for ( int x = 0; x < rawimg.getFirst(); x++) {
-                                bitmap.setPixel(x, y, ints.read());
-                            }
-                        }
-                        return new DefaultBitmapHolder(bitmap);
-                    }
-                    finally {
-                        ints.close();
-                    }
-                }
-            }
-            catch (Exception e) {
-                LOG.warn("exception when using simpleimage-lite to decode jpeg, detail: {}", 
-                        ExceptionUtils.exception2detail(e));
-            }
-            return null;
-        }
-        else*/ {
-            LOG.info("try using BitmapFactory.decodeStream");
-            
-            final Bitmap bitmap = ( null == opts ? BitmapFactory.decodeStream(is) 
-                    : BitmapFactory.decodeStream(is, null, opts));
-            return (null != bitmap ? new DefaultBitmapHolder(bitmap) : null);
-        }
     }
 }
